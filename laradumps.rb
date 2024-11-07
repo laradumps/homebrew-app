9f120d682f6f2821a87dc583a6a4d7eb7c15b03e
luanfreitasdev <luanfreitas10@protonmail.com>
2024-11-07 16:27:17 -0300

    wip

---
+++ laradumps.rb
@@ -0,0 +1,18 @@
+cask "laradumps" do
+  version "3.4.1"
+  sha256 "8b555135faac703a8f0f5987618b2f9c4db2892915c89f4cc5b53b0215034b7d"
+
+  url "https://github.com/laradumps/app/releases/download/v#{version}/LaraDumps-#{version}-universal-mac.zip",
+      verified: "github.com/laradumps/app/"
+  name "LaraDumps"
+  desc "🛻 LaraDumps is a friendly app designed to boost your Laravel PHP coding and debugging experience."
+  homepage "https://laradumps.dev"
+
+  app "LaraDumps.app"
+
+  zap trash: [
+    "~/Library/Application Support/laradumps",
+    "~/Library/Preferences/com.laradumps.app.plist",
+    "~/Library/Saved Application State/com.laradumps.app.savedState",
+  ]
+end
---
+++ update_cask.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REPO="laradumps/app"
+API_URL="https://api.github.com/repos/$REPO/releases/latest"
+
+LATEST_RELEASE=$(curl -sL "$API_URL")
+VERSION=$(echo "$LATEST_RELEASE" | jq -r '.tag_name' | sed 's/^v//')
+LATEST_URL="https://github.com/$REPO/releases/download/v$VERSION/latest-mac.yml"
+
+echo "Downloading $LATEST_URL..."
+
+curl -sL "$LATEST_URL" -o latest-mac.yaml
+
+ZIP_PATH=$(grep -E "^path:" latest-mac.yaml | awk '{print $2}')
+ZIP_URL="https://github.com/$REPO/releases/download/v$VERSION/$ZIP_PATH"
+
+echo "New version: $VERSION"
+echo "Download URL: $ZIP_URL"
+
+echo "Downloading $ZIP_PATH..."
+
+curl -sL "$ZIP_URL" -o "$ZIP_PATH"
+
+echo "Updating the Cask file (laradumps.rb)..."
+
+SHA256=$(shasum -a 256 "$ZIP_PATH" | awk '{print $1}')
+
+sed -i '' "s/^  version \".*\"/  version \"$VERSION\"/" "laradumps.rb"
+sed -i '' "s/^  sha256 \".*\"/  sha256 \"$SHA256\"/" "laradumps.rb"
+
+rm -f latest-mac.yaml "$ZIP_PATH"
+
+echo "Update complete!"
