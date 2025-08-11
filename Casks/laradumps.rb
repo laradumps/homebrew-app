97061a488e333d3eb21a2bbca998c609d7462fe9
luanfreitasdev <luanfreitas10@protonmail.com>
2025-08-11 17:25:33 -0300

    v4.6.0

--- Casks/laradumps.rb
+++ Casks/laradumps.rb
@@ -1,6 +1,6 @@
 cask "laradumps" do
-  version "4.5.3"
-  sha256 "088a1c521a1f2ff25ee1de3515f3c13262098a4a3784282e989dc569d0de1854"
+  version "4.6.0"
+  sha256 "fd6a8ab28470bf60b1db86bf5d49fa52a657400d6ab2047c93fbaf95b0f5e6e1"
 
   url "https://github.com/laradumps/app/releases/download/v#{version}/LaraDumps-#{version}-universal-mac.zip",
       verified: "github.com/laradumps/app/"
