99d2ec934d469a16d8e26375561b36a461b9aa50
Felix <felix.carvalho@devsquad.com>
2026-09-14 09:41:53 -0300

    fix: remove deprecated verified param from cask url

--- Casks/laradumps.rb
+++ Casks/laradumps.rb
@@ -2,8 +2,7 @@ cask "laradumps" do
   version "4.18.0"
   sha256 "b040874352193bff240cde3ffa850141cac9372f1c7b4477d2dab5e641b7a526"
 
-  url "https://github.com/laradumps/app/releases/download/v#{version}/LaraDumps-#{version}-universal-mac.zip",
-      verified: "github.com/laradumps/app/"
+  url "https://github.com/laradumps/app/releases/download/v#{version}/LaraDumps-#{version}-universal-mac.zip"
   name "LaraDumps"
   desc "🛻 LaraDumps is a friendly app designed to boost your Laravel PHP coding and debugging experience."
   homepage "https://laradumps.dev"
